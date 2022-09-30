d15f9a3768247a8a9bbfe1436d2a846d8e994345
Alex <alex@g.com>
2022-10-01 00:18:10 +0300

    work

--- buttons.js
+++ buttons.js
@@ -4,10 +4,15 @@ var buttOn = document.querySelectorAll('.btnjs');
 var bloghidden = function () {
     blog.forEach (v => v.classList.add('hidden')); 
   };
+var buttonhidden = function () {
+    buttOn.forEach (v => v.classList.remove('bg-[#FFCC73]','text-black'));
+  };
 var addKeyClickHandler = function (buttOns, blogs) {
         buttOns.addEventListener('click', function () {
             bloghidden();
+            buttonhidden();
            blogs.classList.remove('hidden');
+           buttOns.classList.add('bg-[#FFCC73]','text-black');
           });
     };    
     for (var i = 0; i < buttOn.length; i++) {
